e37f21aa3564d35d0ef2ed93548da7d18b7144b6
nisargmakwana <nisargmakwana22@gmail.com>
2024-02-08 01:06:29 +0530

    import files fixed

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import clouds from "url:../img/clouds.png";
+import cloudyMoon from "url:../img/cloudy-moon.png";
+import cloudySun from "url:../img/cloudy-sun.png";
+import fog from "url:../img/fog.png";
+import heavyRain from "url:../img/heavy-rain.png";
+import lightRain from "url:../img/light-rain.png";
+import moon from "url:../img/moon.png";
+import sun from "url:../img/sun.png";
+import snow from "url:../img/snow.png";
+import thunderstorm from "url:../img/thunderstorm.png";
+
+export {
+	clouds,
+	cloudyMoon,
+	cloudySun,
+	fog,
+	heavyRain,
+	lightRain,
+	moon,
+	sun,
+	snow,
+	thunderstorm,
+};
--- src/script.js
+++ src/script.js
@@ -2,16 +2,18 @@
 import { ICON_MAP_DAY } from "./iconMap-d.js";
 import { ICON_MAP_NIGHT } from "./iconMap-n.js";
 import { WEATHER_CODE_MAP } from "./weatherCodeMap.js";
-import clouds from "url:../img/clouds.png";
-import cloudyMoon from "url:../img/cloudy-moon.png";
-import cloudySun from "url:../img/cloudy-sun.png";
-import fog from "url:../img/fog.png";
-import heavyRain from "url:../img/heavy-rain.png";
-import lightRain from "url:../img/light-rain.png";
-import moon from "url:../img/moon.png";
-import sun from "url:../img/sun.png";
-import snow from "url:../img/snow.png";
-import thunderstorm from "url:../img/thunderstorm.png";
+import {
+	clouds,
+	cloudyMoon,
+	cloudySun,
+	fog,
+	heavyRain,
+	lightRain,
+	moon,
+	sun,
+	snow,
+	thunderstorm,
+} from "./index.js";
 
 // Elements
 const weatherIcon = document.querySelector(".weather-icon");
